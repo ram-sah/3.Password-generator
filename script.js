252eb781b0a30e340c6c475d27fcb96d33f59437
Ram Sah <mohan2036@gmail.com>
2020-06-11 15:06:51 -0400

    changed the position of for loop

--- script.js
+++ script.js
@@ -5,22 +5,25 @@ var generateBtn = document.querySelector("#generate");
 var upperEl = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerEl = "abcdefghijklmnopqrstuvwxyz";
 var specialEl = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~«µ¶¿¥æ®¤¢†©ð";
-var numEl = "01234567890";
-
-//Creating an object to save users choices
-var userChoices = {};
+var numEl = "0123456789";
+var values= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~«µ¶¿¥æ®¤¢†©ð";
 
 //get user input 
-function generatePassword (){
   var charLength = prompt("How many characters do you want to generate select (8 to 128) ?");
   if (charLength >= 8 && charLength <= 128) {
   var upperCase= confirm("Click ok to confirm Upper Cases:");
   var lowerCase= confirm("Click ok to confirm Lower Cases:");
   var specChar = confirm("Click ok to confirm SpecialCharacters:") ;
   var numChar = confirm("Click ok to confirm Numerical Characters:") ;
-  } else {
-    alert("you need to choose 8 to 128 characters click (generate password) button to start over");
+  } 
+  else {
+    alert("you need to choose 8 to 128 characters click refresh to start over");
   }
+  
+//Creating an object to save users choices
+var userChoices = {};
+
+function generatePassword (){
   // Saving user's answers from prompted questions
   if(upperCase){
     userChoices["upperEl"] = upperEl;
@@ -57,7 +60,7 @@ for (var i = 0; i < charLength; i++){
 }
 return finalPassword;
 }
-
+/*
 function getRandomCharacter(str){
   return str[Math.floor(Math.random()* str.length)];
 }
@@ -67,3 +70,4 @@ function getRandomCharacter(str){
     passwordText.value = password;
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+*/

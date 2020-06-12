66d76f3a0410bd8f3b3701f683e0b8c7d36a4a29
Ram Sah <mohan2036@gmail.com>
2020-06-11 23:57:22 -0400

    moved user input in function and add function completeWritePassword

--- script.js
+++ script.js
@@ -6,68 +6,74 @@ var upperEl = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerEl = "abcdefghijklmnopqrstuvwxyz";
 var specialEl = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~«µ¶¿¥æ®¤¢†©ð";
 var numEl = "0123456789";
-var values= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~«µ¶¿¥æ®¤¢†©ð";
+//var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~«µ¶¿¥æ®¤¢†©ð";
 
 //get user input 
-  var charLength = prompt("How many characters do you want to generate select (8 to 128) ?");
-  if (charLength >= 8 && charLength <= 128) {
-  var upperCase= confirm("Click ok to confirm Upper Cases:");
-  var lowerCase= confirm("Click ok to confirm Lower Cases:");
-  var specChar = confirm("Click ok to confirm SpecialCharacters:") ;
-  var numChar = confirm("Click ok to confirm Numerical Characters:") ;
-  } 
-  else {
-    alert("you need to choose 8 to 128 characters click refresh to start over");
-  }
-  
+var charLength;
+
 //Creating an object to save users choices
 var userChoices = {};
 
-function generatePassword (){
+function generatePassword() {
+  charLength = prompt("How many characters do you want to generate select (8 to 128) ?");
+  if (charLength >= 8 && charLength <= 128) {
+    var upperCase = confirm("Click ok to confirm Upper Cases:");
+    var lowerCase = confirm("Click ok to confirm Lower Cases:");
+    var specChar = confirm("Click ok to confirm SpecialCharacters:");
+    var numChar = confirm("Click ok to confirm Numerical Characters:");
+  }
+  else {
+    alert("you need to choose 8 to 128 characters click refresh to start over");
+  }
   // Saving user's answers from prompted questions
-  if(upperCase){
+  if (upperCase) {
     userChoices["upperEl"] = upperEl;
   }
-  if (lowerCase){
-    userChoices["lowerEl"] = lowerEl;    
+  if (lowerCase) {
+    userChoices["lowerEl"] = lowerEl;
   }
-  if (specChar){
+  if (specChar) {
     userChoices["specialEl"] = specialEl;
   }
-  if (numChar){
+  if (numChar) {
     userChoices["numEl"] = numEl;
   }
   console.log(userChoices);
-  
+
 }
 // calling the generatePassword function
-  //generatePassword();
+//generatePassword();
 
 // Write password to the #password input
 function writePassword() {
-let finalPassword = "";
-for (var i = 0; i < charLength; i++){
+  let finalPassword = "";
+  for (var i = 0; i < charLength; i++) {
 
-  let passwordEls = Object.keys(userChoices);
-  let randonEls = passwordEls[Math.floor(Math.random() * passwordEls.length)];
+    let passwordEls = Object.keys(userChoices);
+    let randonEls = userChoices[passwordEls[Math.floor(Math.random() * passwordEls.length)]];
 
-  console.log(randonEls);
-  
-  let char = getRandomCharacter(userChoices[randonEls]);
-  finalPassword += char;
+    console.log(randonEls);
 
-  console.log(char);
+    let char = getRandomCharacter(randonEls);
+    finalPassword += char;
+
+    console.log(char);
+  }
+  return finalPassword;
 }
-return finalPassword;
-}
-/*
-function getRandomCharacter(str){
-  return str[Math.floor(Math.random()* str.length)];
-}
-   var password = generatePassword();
-   var passwordText = document.querySelector("#password");
 
-    passwordText.value = password;
+function getRandomCharacter(str) {
+  return str[Math.floor(Math.random() * str.length)];
+}
+
+function completeWritePassword() {
+  generatePassword();
+  var password = writePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+  userChoices = {};
+}
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-*/
+generateBtn.addEventListener("click", completeWritePassword);
